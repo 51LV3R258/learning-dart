edeaa3bf1fe0d53d56a85baef6bcb8efcc9a2ddd
51lv3r258 <jrp8900@gmail.com>
2021-09-20 18:03:51 -0500

    Abstract Class

---
+++ abstractClass/main.dart
@@ -0,0 +1,31 @@
+
+void main() {
+  final perro = new Perro();
+  final gato = new Gato();
+
+  sonidoAnimal(perro);
+  sonidoAnimal(gato);
+}
+
+void sonidoAnimal(Animal animal) {
+  animal.emitirSonido();
+}
+
+abstract class Animal {
+  int? patas;
+
+  void emitirSonido();
+}
+
+class Perro implements Animal {
+  int? patas;
+
+  void emitirSonido() => print('Guauu Guau');
+}
+
+class Gato implements Animal {
+  int? patas;
+  int? cola;
+
+  void emitirSonido() => print('Miau Miau');
+}
